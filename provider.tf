cbe2263fa6172ac16d4a40147b8ad75aa3efa255
bkchaudhari <96370111+bkchaudhari@users.noreply.github.com>
2023-04-02 15:49:12 +0530

    update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
 }
 provider "citrixadc" {
-  endpoint = "http://172.16.136.10"
+  endpoint = "http://172.16.149.100"
   username = var.ns_username
   password = var.ns_password
 }
