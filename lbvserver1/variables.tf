acf140bc45aae423917a8a285e6b391a1668bee9
bkchaudhari <chaudharibk1985@outlook.com>
2023-04-10 11:55:29 +0530

    tf update

--- lbvserver1/variables.tf
+++ lbvserver1/variables.tf
@@ -1,4 +1,4 @@
-variable "ns_username" {
+/*variable "ns_username" {
   description = "ADC administrator username"
   type        = string
   sensitive   = true
@@ -7,4 +7,4 @@ variable "ns_password" {
   description = "ADC administrator password"
   type        = string
   sensitive   = true
-}
+}*/
